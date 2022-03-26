0f58795882653b979a7f115f856e2102b45cfcfc
AKSHAYSAHU366 <76153834+AKSHAYSAHU366@users.noreply.github.com>
2022-03-26 13:02:49 +0530

    Time: 1 ms (98.10%), Space: 45.3 MB (38.65%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.java
+++ 260-single-number-iii/260-single-number-iii.java
@@ -2,32 +2,27 @@ class Solution {
     public int[] singleNumber(int[] nums)
     {
         int sum = 0;
-        for (int i=0;i<nums.length;i++)
+        for (int i = 0; i<nums.length;i++)
         {
             sum = (sum^nums[i]);
         }
-        sum = (sum &-sum);
+        sum = (sum&-sum);
         int sum1 = 0;
         int sum2 = 0;
-      for(int i = 0;i<nums.length;i++)
+        for(int i =0;i<nums.length;i++)
         {
-            if((nums[i]&sum)==0)
+            if((sum & nums[i])==0)
             {
-                 sum1 = sum1^nums[i];
-                
+                sum1 = sum1^nums[i];
             }
             else
             {
-                 sum2 = sum2^nums[i];
+                sum2 = sum2^nums[i];
             }
         }
-       
-      
-        int arr[] = new int[2];
+        int arr[]= new int[2];
         arr[0] = sum1;
         arr[1] = sum2;
-        
         return arr;
     }
 }
-
